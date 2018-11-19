bfb4e028f8247eca52cb2abe94485629b488ce9c
HenryKenya <henry.onyango@outlook.com>
2018-11-19 16:44:03 +0300

    - Create data class - Data binding for performance

--- app/src/main/java/ke/co/skylinedesign/aboutme/MainActivity.kt
+++ app/src/main/java/ke/co/skylinedesign/aboutme/MainActivity.kt
@@ -1,12 +1,44 @@
 package ke.co.skylinedesign.aboutme
 
+import android.content.Context
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import ke.co.skylinedesign.aboutme.databinding.ActivitiyMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitiyMainBinding
 
+    private val myName: MyName = MyName("Henry Onyango")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activitiy_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activitiy_main)
+        binding.myName = myName
+        binding.doneButton.setOnClickListener {
+            addNickName(it)
+        }
     }
+
+    private fun addNickName(view: View) {
+
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+
+    }
+
+
 }
